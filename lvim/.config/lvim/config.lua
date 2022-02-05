3afa44ce246217d7d49dd2c3babdba4c8559075d
yong1le <ygle.he@gmail.com>
2022-02-05 11:06:38 -0500

    add formatter settings

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -19,7 +19,6 @@ lvim.keys.normal_mode["``"] = ":ToggleTerm<cr>"
 lvim.keys.normal_mode["<TAB>"] = ":BufferNext<cr>"
 lvim.keys.normal_mode["<S-TAB>"] = ":BufferPrevious<cr>"
 
-
 -- Running code
 _G.run_code = function()
 	local ft = vim.api.nvim_buf_get_option(0, "filetype")
@@ -45,7 +44,7 @@ end
 vim.api.nvim_set_keymap("n", "<space>r", "v:lua.run_code()", { expr = true })
 
 -- Whichkey
-lvim.builtin.which_key.mappings["G"] = {"<cmd>TermExec cmd=\"lazygit\"<cr>", "Lazygit"}
+lvim.builtin.which_key.mappings["G"] = { '<cmd>TermExec cmd="lazygit"<cr>', "Lazygit" }
 
 -- Dashboard
 lvim.builtin.dashboard.active = true
@@ -63,82 +62,82 @@ lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 
 -- Nvim Tree
-local tree_cb = require "nvim-tree.config".nvim_tree_callback
+local tree_cb = require("nvim-tree.config").nvim_tree_callback
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 1
 lvim.builtin.nvimtree.setup = {
- disable_netrw = true,
-  hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = {},
-  auto_close = true,
-  open_on_tab = false,
-  hijack_cursor = false,
-  update_cwd = true,
-  update_to_buf_dir = {
-    enable = true,
-    auto_open = true
-  },
-  diagnostics = {
-    enable = false,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = ""
-    }
-  },
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-    ignore_list = {}
-  },
-  system_open = {
-    cmd = nil,
-    args = {}
-  },
-  filters = {
-    dotfiles = true,
-    custom = {"*.exe", "*.o", "*.out"}
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 500
-  },
-  view = {
-    width = 30,
-    height = 30,
-    hide_root_folder = false,
-    side = "left",
-    auto_resize = false,
-    mappings = {
-      custom_only = false,
-      list = {
-        {key = {"<2-RightMouse>", "<C-}>", "e"}, cb = tree_cb("cd")}
-      }
-    },
-    number = false,
-    relativenumber = false,
-    signcolumn = "yes"
-  },
-  trash = {
-    cmd = "trash",
-    require_confirm = true
-  }
+	disable_netrw = true,
+	hijack_netrw = true,
+	open_on_setup = false,
+	ignore_ft_on_setup = {},
+	auto_close = true,
+	open_on_tab = false,
+	hijack_cursor = false,
+	update_cwd = true,
+	update_to_buf_dir = {
+		enable = true,
+		auto_open = true,
+	},
+	diagnostics = {
+		enable = false,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+		ignore_list = {},
+	},
+	system_open = {
+		cmd = nil,
+		args = {},
+	},
+	filters = {
+		dotfiles = true,
+		custom = { "*.exe", "*.o", "*.out" },
+	},
+	git = {
+		enable = true,
+		ignore = true,
+		timeout = 500,
+	},
+	view = {
+		width = 30,
+		height = 30,
+		hide_root_folder = false,
+		side = "left",
+		auto_resize = false,
+		mappings = {
+			custom_only = false,
+			list = {
+				{ key = { "<2-RightMouse>", "<C-}>", "e" }, cb = tree_cb("cd") },
+			},
+		},
+		number = false,
+		relativenumber = false,
+		signcolumn = "yes",
+	},
+	trash = {
+		cmd = "trash",
+		require_confirm = true,
+	},
 }
 
 -- Treesitter
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "css",
-  "java",
+	"bash",
+	"c",
+	"javascript",
+	"json",
+	"lua",
+	"python",
+	"typescript",
+	"css",
+	"java",
 }
 
 lvim.builtin.treesitter.highlight.enabled = true
@@ -149,10 +148,10 @@ lvim.builtin.lualine.style = "lvim"
 
 -- Additional Plugins
 lvim.plugins = {
-    {
-      "tpope/vim-surround",
-      keys = {"c", "d", "y"}
-    }
+	{
+		"tpope/vim-surround",
+		keys = { "c", "d", "y" },
+	},
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
@@ -160,45 +159,20 @@ lvim.plugins = {
 --   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
 -- }
 
-
 -- generic LSP settings
 
--- ---@usage disable automatic installation of servers
--- lvim.lsp.automatic_servers_installation = false
-
--- ---@usage Select which servers should be configured manually. Requires `:LvimCacheRest` to take effect.
--- See the full default list `:lua print(vim.inspect(lvim.lsp.override))`
--- vim.list_extend(lvim.lsp.override, { "pyright" })
-
--- ---@usage setup a server -- see: https://www.lunarvim.org/languages/#overriding-the-default-configuration
--- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pylsp", opts)
-
--- -- you can set a custom on_attach function that will be used for all the language servers
--- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
-
 -- -- set a formatter, this will override the language server formatting capabilities (if it exists)
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   { command = "black", filetypes = { "python" } },
---   { command = "isort", filetypes = { "python" } },
---   {
---     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
---     command = "prettier",
---     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     extra_args = { "--print-with", "100" },
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "typescript", "typescriptreact" },
---   },
--- }
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup({
+	{ command = "black", filetypes = { "python" } },
+	{ command = "stylua", filetypes = { "lua" } },
+	{ command = "clang-format", filetypes = { "c++", "java" } },
+	{
+		command = "prettier",
+		extra_args = { "--print-with", "100" },
+		filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "html", "css", "json" },
+	},
+})
 
 -- -- set additional linters
 -- local linters = require "lvim.lsp.null-ls.linters"
