98e8a4349e6c30d47740f6181a015933d7a4967d
yong1le <ygle.he@gmail.com>
2024-06-08 12:21:14 -0400

    add ts to vsvim

--- vsvim/.config/vsvim/init.lua
+++ vsvim/.config/vsvim/init.lua
@@ -6,81 +6,139 @@ vim.opt.ignorecase = true
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 autocmd("TextYankPost", {
-  desc = "Highlight yanked text",
-  group = augroup("Highlight Yank", { clear = true }),
-  pattern = "*",
-  callback = function() vim.highlight.on_yank() end,
+	desc = "Highlight yanked text",
+	group = augroup("Highlight Yank", { clear = true }),
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazypath,
-  }
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ','
-require('lazy').setup {
-  {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    opts = {},
-    keys = {
-      { "s", mode = { "n", "x", "o" }, "<cmd>lua require('flash').jump()<cr>", desc = "Flash" },
-      { "S", mode = { "n", "o", "x" }, "<cmd>lua require('flash').treesitter() <cr>", desc = "Flash Treesitter" },
-    },
-  },
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+require("lazy").setup({
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+			{ "s", mode = { "n", "x", "o" }, "<cmd>lua require('flash').jump()<cr>", desc = "Flash" },
+			{ "S", mode = { "n", "o", "x" }, "<cmd>lua require('flash').treesitter() <cr>", desc = "Flash Treesitter" },
+		},
+	},
 
-  {
-    "kylechui/nvim-surround",
-    version = "*",
-    event = "VeryLazy",
-    opts = {
-      keymaps = {
-        insert = "<C-g>z",
-        insert_line = "<C-g>Z",
-        normal = "yz",
-        normal_cur = "yzz",
-        normal_line = "yZ",
-        normal_cur_line = "yZZ",
-        visual = "Z",
-        visual_line = "gZ",
-        delete = "dz",
-        change = "cz",
-        change_line = "cZ",
-      },
-    },
-    config = function(_, opts)
-      require("nvim-surround").setup(opts)
-    end
-  },
-  defaults = { lazy = true, version = nil },
-  install = { missing = false },
-  checker = { enabled = false },
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        'gzip',
-        'matchit',
-        'matchparen',
-        'netrwPlugin',
-        'tarPlugin',
-        'tohtml',
-        'tutor',
-        'zipPlugin',
-      },
-    },
-  },
-}
+	{
+		"kylechui/nvim-surround",
+		version = "*",
+		event = "VeryLazy",
+		opts = {
+			keymaps = {
+				insert = "<C-g>z",
+				insert_line = "<C-g>Z",
+				normal = "yz",
+				normal_cur = "yzz",
+				normal_line = "yZ",
+				normal_cur_line = "yZZ",
+				visual = "Z",
+				visual_line = "gZ",
+				delete = "dz",
+				change = "cz",
+				change_line = "cZ",
+			},
+		},
+		config = function(_, opts)
+			require("nvim-surround").setup(opts)
+		end,
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = { "BufReadPost", "BufNewFile" },
+		opts = {
+			ensure_installed = "all",
+			auto_install = true,
+			autotag = { enable = false },
+			highlight = { enable = false },
+			incremental_selection = { enable = false },
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = { query = "@function.outer", desc = "Select around function" },
+						["if"] = { query = "@function.inner", desc = "Select inside function" },
+						["ac"] = { query = "@class.outer", desc = "Select around class" },
+						["ic"] = { query = "@class.inner", desc = "Select inside class" },
+						["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+					},
+					include_surrounding_whitespace = true,
+				},
+			},
+			indent = { enable = false },
+		},
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+		end,
+	},
+
+	-- Extend textobjects using TS
+	{
+		"echasnovski/mini.ai",
+		event = "VeryLazy",
+		version = false,
+		opts = {
+			custom_textobjects = nil,
+			mappings = {
+				around = "a",
+				inside = "i",
+				around_next = "an",
+				inside_next = "in",
+				around_last = "al",
+				inside_last = "il",
+
+				-- Move cursor to corresponding edge of `a` textobject
+				goto_left = "g[",
+				goto_right = "g]",
+			},
+			n_lines = 50,
+			search_method = "cover_or_next",
+			silent = false,
+		},
+	},
+	defaults = { lazy = true, version = nil },
+	install = { missing = false },
+	checker = { enabled = false },
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"matchit",
+				"matchparen",
+				"netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
+})
 
 -- calling whichkey
-vim.cmd[[
+vim.cmd([[
   nnoremap <silent> <Space> :call VSCodeNotify('whichkey.show')<CR>
   xnoremap <silent> <Space> :call VSCodeNotify('whichkey.show')<CR>
-]]
+]])
