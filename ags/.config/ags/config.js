44f132a4318e7e5c556675b0d830b45c5a0e45d1
Yong Le He <ygle.he@gmail.com>
2024-07-14 13:22:24 -0400

    swaync

--- ags/.config/ags/config.js
+++ ags/.config/ags/config.js
@@ -1,20 +1,20 @@
 // import OSD from "./widgets/OSD/OSD.js";
 // import Bar from "./widgets/Bar/Bar.js";
 // import ControlCentre from "./widgets/ControlCentre/ControlCentre.js";
-import NotificationPopups from "./widgets/Notification/Notification.js";
+// import NotificationPopups from "./widgets/Notification/Notification.js";
 // import PopupCalendar from "./widgets/Calendar/Calendar.js";
 import AppLauncher from "./widgets/AppLauncher/AppLauncher.js";
 
-const hyprland = await Service.import("hyprland");
+// const hyprland = await Service.import("hyprland");
 //
 // import setupGlobals from "./scripts/globals.js";
 
 // setupGlobals();
 
 App.config({
-  style: "./style.css",
+  // style: "./style.css",
   windows: [
-    hyprland.monitors.map((m) => NotificationPopups(m.id)),
+    // hyprland.monitors.map((m) => NotificationPopups(m.id)),
     // hyprland.monitors.map((m) => Bar(m.id)),
     // ControlCentre(),
     // OSD("volume"),
