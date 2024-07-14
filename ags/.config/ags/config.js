4eded28e8590fc67823c29e39557f36b46cf3df2
Yong Le He <ygle.he@gmail.com>
2024-07-14 13:08:39 -0400

    use ags for app launcher and notif

--- ags/.config/ags/config.js
+++ ags/.config/ags/config.js
@@ -1,27 +1,27 @@
-import OSD from "./widgets/OSD/OSD.js";
-import Bar from "./widgets/Bar/Bar.js";
-import ControlCentre from "./widgets/ControlCentre/ControlCentre.js";
+// import OSD from "./widgets/OSD/OSD.js";
+// import Bar from "./widgets/Bar/Bar.js";
+// import ControlCentre from "./widgets/ControlCentre/ControlCentre.js";
 import NotificationPopups from "./widgets/Notification/Notification.js";
-import PopupCalendar from "./widgets/Calendar/Calendar.js";
+// import PopupCalendar from "./widgets/Calendar/Calendar.js";
 import AppLauncher from "./widgets/AppLauncher/AppLauncher.js";
 
 const hyprland = await Service.import("hyprland");
+//
+// import setupGlobals from "./scripts/globals.js";
 
-import setupGlobals from "./scripts/globals.js";
-
-setupGlobals();
+// setupGlobals();
 
 App.config({
   style: "./style.css",
   windows: [
-    hyprland.monitors.map((m) => Bar(m.id)),
-    ControlCentre(),
     hyprland.monitors.map((m) => NotificationPopups(m.id)),
-    OSD("volume"),
-    OSD("brightness"),
-    OSD("mic"),
-    OSD("ppd"),
-    PopupCalendar(),
+    // hyprland.monitors.map((m) => Bar(m.id)),
+    // ControlCentre(),
+    // OSD("volume"),
+    // OSD("brightness"),
+    // OSD("mic"),
+    // OSD("ppd"),
+    // PopupCalendar(),
     AppLauncher(),
   ],
 });
