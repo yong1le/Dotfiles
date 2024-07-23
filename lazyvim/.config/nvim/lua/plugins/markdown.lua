b0ff87617047e071de30fe98253e4133ed1b0606
Yong Le He <ygle.he@gmail.com>
2024-07-22 20:17:41 -0400

    add obsidian.nvim

--- lazyvim/.config/nvim/lua/plugins/markdown.lua
+++ lazyvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,5 +1,24 @@
 return {
   {
     "MeanderingProgrammer/markdown.nvim",
+    main = "render-markdown",
+    opts = {},
+    ft = "markdown",
+  },
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",
+    lazy = true,
+    event = {
+      "BufReadPre " .. vim.fn.expand("~/Documents/Obsidian Vault/"),
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/Documents/Obsidian Vault",
+        },
+      },
+    },
   },
 }
