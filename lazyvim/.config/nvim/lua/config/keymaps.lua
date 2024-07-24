d231ddbbb74ff3d288051385cc364988e88a0a9c
Yong Le He <ygle.he@gmail.com>
2024-07-23 21:16:53 -0400

    rounded corners

--- lazyvim/.config/nvim/lua/config/keymaps.lua
+++ lazyvim/.config/nvim/lua/config/keymaps.lua
@@ -4,44 +4,47 @@ local set = vim.keymap.set
 ---------------------------------------------------------------------------
 -- DELETE DEFAULT BINDINGS
 ---------------------------------------------------------------------------
-del("n", "<leader>l") -- :Lazy
-del("n", "<leader>L") -- :Lazy
-del("n", "<leader>K") -- hover doc
-del("n", "<leader>`") -- switch to prev buffer
-del("n", "<leader>fn") -- new file
--- del("n", "<leader>xl") -- location list
--- del("n", "<leader>xq") -- quick list
--- del("n", "<leader>cf") -- format
--- del("n", "<leader>cd") -- diagnostic hover
-del("n", "<leader>ft") -- LazyVim.terminal @ LazyVim.root()
-del("n", "<c-/>") -- LazyVim.terminal @ LazyVim.root()
-del("n", "<c-_>") -- LazyVim.terminal @ LazyVim.root()
-del("n", "<leader>fT") -- LazyVim.terminal
-del("t", "<c-/>")
-del("t", "<c-_>")
-del("n", "<leader>|") -- vsplit
-del("n", "<leader>-") -- split
-del("n", "<leader>?") -- buffer keymaps
 
--- TABS
-del("n", "<leader><tab>l")
-del("n", "<leader><tab>o")
-del("n", "<leader><tab>f")
-del("n", "<leader><tab><tab>")
-del("n", "<leader><tab>]")
-del("n", "<leader><tab>d")
-del("n", "<leader><tab>[")
+if not vim.g.vscode then
+  del("n", "<leader>l") -- :Lazy
+  del("n", "<leader>L") -- :Lazy
+  del("n", "<leader>K") -- hover doc
+  del("n", "<leader>`") -- switch to prev buffer
+  del("n", "<leader>fn") -- new file
+  -- del("n", "<leader>xl") -- location list
+  -- del("n", "<leader>xq") -- quick list
+  -- del("n", "<leader>cf") -- format
+  -- del("n", "<leader>cd") -- diagnostic hover
+  del("n", "<leader>ft") -- LazyVim.terminal @ LazyVim.root()
+  del("n", "<c-/>") -- LazyVim.terminal @ LazyVim.root()
+  del("n", "<c-_>") -- LazyVim.terminal @ LazyVim.root()
+  del("n", "<leader>fT") -- LazyVim.terminal
+  del("t", "<c-/>")
+  del("t", "<c-_>")
+  del("n", "<leader>|") -- vsplit
+  del("n", "<leader>-") -- split
+  del("n", "<leader>?") -- buffer keymaps
 
--- Lazygit
-del("n", "<leader>gG")
-del("n", "<leader>gb")
-del("n", "<leader>gl")
-del("n", "<leader>gL")
+  -- TABS
+  del("n", "<leader><tab>l")
+  del("n", "<leader><tab>o")
+  del("n", "<leader><tab>f")
+  del("n", "<leader><tab><tab>")
+  del("n", "<leader><tab>]")
+  del("n", "<leader><tab>d")
+  del("n", "<leader><tab>[")
 
-del("n", "<leader>xl") -- location list
-del("n", "<leader>xq") -- quickfix list
-del("n", "<leader>xt") -- todo list
-del("n", "<leader>xT") -- todo list
+  -- Lazygit
+  del("n", "<leader>gG")
+  del("n", "<leader>gb")
+  del("n", "<leader>gl")
+  del("n", "<leader>gL")
+
+  del("n", "<leader>xl") -- location list
+  del("n", "<leader>xq") -- quickfix list
+  del("n", "<leader>xt") -- todo list
+  del("n", "<leader>xT") -- todo list
+end
 
 ---------------------------------------------------------------------------
 -- SET BINDINGS
