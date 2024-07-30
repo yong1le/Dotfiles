d7a0b7edcf722efe2f70e079eaec9d35d8abc0c5
Yong Le He <ygle.he@gmail.com>
2024-07-29 21:07:44 -0400

    update

--- lazyvim/.config/nvim/lua/config/lazy.lua
+++ lazyvim/.config/nvim/lua/config/lazy.lua
@@ -24,7 +24,7 @@ require("lazy").setup({
     version = false,
   },
   install = { colorscheme = { "kanagawa" } },
-  checker = { enabled = true },
+  checker = { enabled = false },
   performance = {
     rtp = {
       disabled_plugins = {
