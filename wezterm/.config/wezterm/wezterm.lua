8f28263d61b67fddeb8e229de9f365e524a33fbb
Yong Le He <ygle.he@gmail.com>
2024-08-09 08:44:52 -0400

    change wezterm theme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -8,7 +8,7 @@ local config = {}
 if wezterm.config_builder then config = wezterm.config_builder() end
 
 -- Theming
-config.color_scheme = "Gruvbox Material (Gogh)"
+config.color_scheme = "Gruvbox Dark (Gogh)"
 
 -- Fonts
 config.font = wezterm.font "JetBrainsMono Nerd Font"
