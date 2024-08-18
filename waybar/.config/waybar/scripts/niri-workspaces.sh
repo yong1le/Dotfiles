0e40b413b38e333301021169404de29874194ab8
Yong Le He <ygle.he@gmail.com>
2024-08-18 13:22:22 -0400

    niri too

---
+++ waybar/.config/waybar/scripts/niri-workspaces.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+case "$1" in
+focus*)
+  niri msg action "$@" && pkill -SIGRTMIN+8 waybar
+  ;;
+move*)
+  niri msg action "$@" && pkill -SIGRTMIN+8 waybar
+  ;;
+*)
+  set -e
+
+  monitor=$1
+
+  active=""
+  inactive=""
+
+  workspace_str=""
+
+  msg_out="$(niri msg -j workspaces | jq ".[] | select(.output == \"$monitor\") | .is_active")"
+  for ws in $msg_out; do
+    if "$ws"; then
+      workspace_str="${workspace_str}${active}  "
+    else
+      workspace_str="${workspace_str}${inactive}  "
+    fi
+  done
+
+  printf "%s" "$workspace_str"
+  ;;
+esac
