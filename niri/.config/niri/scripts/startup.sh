b39ae6063ba431c698dd4c1ae0d83146b3668560
Yong Le He <ygle.he@gmail.com>
2024-08-18 21:52:28 -0400

    minor tweaks

--- niri/.config/niri/scripts/startup.sh
+++ niri/.config/niri/scripts/startup.sh
@@ -6,8 +6,5 @@ gtk-launch dev.vencord.Vesktop.desktop
 
 sleep 1
 
-niri msg action focus-workspace 2
+~/.config/waybar/scripts/niri-workspaces.sh focus-workspace 2
 gtk-launch org.mozilla.firefox.desktop
-
-~/.config/waybar/scripts/niri-workspaces.sh DP-2
-~/.config/waybar/scripts/niri-workspaces.sh eDP-1
