a8510a272515780e6000ef1ef0233d212e60b0d9
Yong Le He <ygle.he@gmail.com>
2024-08-18 21:35:58 -0400

    niri workspaces waybar

--- waybar/.config/waybar/scripts/niri-workspaces.sh
+++ waybar/.config/waybar/scripts/niri-workspaces.sh
@@ -12,14 +12,25 @@ move*)
 
   monitor=$1
 
-  active=""
+  active="<span color='#a6e3a1'></span>"
   inactive=""
+
+  active_social="<span color='#a6e3a1'>󰓇</span>"
+  inactive_social="󰓇"
 
   workspace_str=""
 
-  msg_out="$(niri msg -j workspaces | jq ".[] | select(.output == \"$monitor\") | .is_active")"
+  msg_out="$(niri msg -j workspaces | jq -c ".[] | select(.output == \"$monitor\") | {is_active: .is_active, name: .name}")"
+
   for ws in $msg_out; do
-    if "$ws"; then
+    is_active="$(echo "$ws" | jq -r '.is_active')"
+    name="$(echo "$ws" | jq -r '.name')"
+
+    if [ "$name" == "social" ] && "$is_active"; then
+      workspace_str="${workspace_str}${active_social}  "
+    elif [ "$name" == "social" ]; then
+      workspace_str="${workspace_str}${inactive_social}  "
+    elif "$is_active"; then
       workspace_str="${workspace_str}${active}  "
     else
       workspace_str="${workspace_str}${inactive}  "
