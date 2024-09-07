39606f2ccef139f90c37107d05e2e931587de8a6
Yong Le He <ygle.he@gmail.com>
2024-09-06 22:35:09 -0400

    updates

--- niri/.config/niri/scripts/startup.sh
+++ niri/.config/niri/scripts/startup.sh
@@ -7,4 +7,4 @@ gtk-launch dev.vencord.Vesktop.desktop
 sleep 1
 
 ~/.config/waybar/scripts/niri-workspaces.sh focus-workspace 2
-flatpak run io.github.zen_browser.zen
+flatpak run org.mozilla.firefox
