3bdd60b8476309ed9395de55571fd7d1ff2afada
Yong Le He <ygle.he@gmail.com>
2024-11-22 12:51:20 -0500

    update

--- lazyvim/.config/nvim/lua/plugins/disabled.lua
+++ lazyvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,4 @@
 return {
-  { "rcarriga/nvim-notify", enabled = false },
   { "tpope/vim-dadbod", enabled = false },
   { "kristijanhusak/vim-dadbod-completion", enabled = false },
   { "kristijanhusak/vim-dadbod-ui", enabled = false },
---
+++ lazyvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          keys = {
+            { icon = " ", key = "f", desc = "Find File", action = "<leader>ff" },
+            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            { icon = " ", key = "g", desc = "Find Text", action = "<leader>sw" },
+            { icon = " ", key = "r", desc = "Recent Files", action = "<leader>fr" },
+            {
+              icon = " ",
+              key = "c",
+              desc = "Config",
+              action = ":lua Snacks.dashboard.pick('', {cwd = vim.fn.stdpath('config')})",
+            },
+            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
+          header = [[
+                                                                    
+      ████ ██████           █████      ██                     
+     ███████████             █████                             
+     █████████ ███████████████████ ███  ███████████   
+    █████████  ███    █████████████ █████ ██████████████   
+   █████████ ██████████ █████████ █████ █████ ████ █████   
+ ███████████ ███    ███ █████████ █████ █████ ████ █████  
+██████  █████████████████████ ████ █████ █████ ████ ██████ 
+        ]],
+        },
+      },
+    },
+  },
+}
