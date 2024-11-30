fa2ad4375d6fc41d15c20d379abcc19ac5e2eb82
Yong Le He <ygle.he@gmail.com>
2024-11-30 10:21:25 -0500

    update nvim latex

---
+++ lazyvim/.config/nvim/lua/plugins/notes.lua
@@ -0,0 +1,38 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {
+    heading = {
+      sign = false,
+      icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰲦 ", "󰎱 ", "󰎳 " },
+      position = "overlay",
+      border = true,
+      left_pad = 1,
+    },
+  },
+  {
+    "lervag/vimtex",
+    lazy = false,
+    config = function()
+      vim.g.vimtex_mappings_disable = { ["n"] = { "K" } }
+      vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+      vim.g.vimtex_view_method = "zathura"
+      vim.g.tex_flavor = "latex"
+      vim.g.vimtex_quickfix_mode = 0
+      vim.g.vimtex_matchparen_enabled = 0
+      vim.g.vimtex_compiler_latexmk = {
+        out_dir = "./outputs",
+        callback = 1,
+        continuous = 1,
+        executable = "latexmk",
+        hooks = {},
+        options = {
+          "-verbose",
+          "-file-line-error",
+          "-synctex=1",
+          "-interaction=nonstopmode",
+          "-shell-escape",
+        },
+      }
+    end,
+  },
+}
