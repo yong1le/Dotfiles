ad629e0fea06324bd058f6f0ddbe2f2a2ec37f2c
Yong Le He <ygle.he@gmail.com>
2025-04-30 21:20:31 -0400

    update

--- lazyvim/.config/nvim/lua/plugins/coding.lua
+++ lazyvim/.config/nvim/lua/plugins/coding.lua
@@ -17,7 +17,6 @@ return {
     opts = {
       formatters_by_ft = {
         lua = { "stylua" },
-        python = { "isort", "black" },
         javascript = { "prettierd", "prettier", stop_after_first = true },
         typescript = { "prettierd", "prettier", stop_after_first = true },
         javascriptreact = { "prettierd", "prettier", stop_after_first = true },
